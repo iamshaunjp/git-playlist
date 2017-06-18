c51295a60bb0e922610410b100c7fc6aff9ee082
Julian Yaman <julianyaman@gmx.de>
2017-06-18 13:42:25 +0200

    Improved ninja.js -> Adding Color to console output

--- ninja.js
+++ ninja.js
@@ -1,1 +1,4 @@
-console.log('do your worst');
+// Ninjas are cool!
+
+console.log('%cDo your worst!', 'background: red; color: white; display: block');
+console.log('%cFork it on GitHub: https://github.com/iamshaunjp/git-playlist.git', 'background: #0057A0; color: white; display: block');
