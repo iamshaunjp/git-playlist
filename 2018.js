2aabbc469f1f698762e933314de17f22b339705f
anthonylan <anthonylannn@gmail.com>
2018-08-16 17:32:32 +0200

    new addos

---
+++ 2018.js
@@ -0,0 +1,16 @@
+
+//add in your name to the array if you watched the tutorial in 2018
+
+let _2018viewers = [
+  {
+    "ninjas": [
+        "Anthonylan"
+
+    ]
+  }
+]
+
+
+_2018viewers.forEach(viewers => {
+  console.log(viewers.ninjas);
+})
