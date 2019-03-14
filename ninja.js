0b1ce8f1c4ae8d6b6a4ba4985efff35852270487
Ankur Kalambe <AK1915@live.mdx.ac.uk>
2019-03-14 14:49:11 +0000

    My first Open scource Contribution

--- ninja.js
+++ ninja.js
@@ -13,6 +13,7 @@ console.log("What's up guys, this is my first pull request! Thanks Shaun!");
 console.log("Great tutorial, Thanks a lot! msamancioglu");
 console.log("Hey Shaun! I really like the tutorial, thanks alot. anthonylan");
 console.log("Thank you");
+console.log("Thank you for helping, this is my first contribution to this open source community and I had to make it here after watching you video <3")
 
 function Track(song, artist, album, year, starReview){
   this.song = song;
