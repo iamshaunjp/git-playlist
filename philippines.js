52076c666f1e11b0d40b899bee0a0cd1a4e95340
shaddao <rkevin051@gmail.com>
2020-07-04 13:53:41 +0800

    added crappy promise

--- philippines.js
+++ philippines.js
@@ -1,1 +1,10 @@
 const thanks = 'Thank you shaun!';
+const promise = new Promise((resolve, reject) => {
+    if (true) {
+        resolve('Resolve');
+    } else {
+        reject('Reject');
+    }
+});
+
+promise.then(e => console.log(e)).catch(e => console.log(e));
