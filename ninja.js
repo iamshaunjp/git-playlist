57f78800126f784c8bb188190f9d6929807c98dc
charles <charles.zheng@msn.com>
2021-04-21 15:27:27 -0400

    Say thank you!

--- ninja.js
+++ ninja.js
@@ -13,6 +13,7 @@ console.log("What's up guys, this is my first pull request! Thanks Shaun!");
 console.log("Great tutorial, Thanks a lot! msamancioglu");
 console.log("Hey Shaun! I really like the tutorial, thanks alot. anthonylan");
 console.log("Thank you");
+console.log('I was struggling for days about github until I found your tutorial!')
 
 function Track(song, artist, album, year, starReview){
   this.song = song;
