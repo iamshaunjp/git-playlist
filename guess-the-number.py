e62c00f6cc4ecc405e2c0f4bc4155f161c1fc66f
Kamal Koranga <kamalkoranga75@gmail.com>
2021-10-15 16:56:06 +0530

    added three python projects

---
+++ guess-the-number.py
@@ -0,0 +1,20 @@
+import random
+
+# ----variables
+num = random.randrange(0, 101)
+user_num = int()
+
+#-----loop
+while num != user_num :
+    user_num = int(input("Guess the number(from 0 to 101): "))
+    if user_num == num:
+        print("-------------You got it!----------")
+    
+    elif user_num > num :
+        print("You have gussed a greater number!")
+
+    elif user_num < num :
+        print("You have gussed a smaller number!")
+
+    else:
+        print("You have gussed larger number than 100!")
