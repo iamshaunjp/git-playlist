0b9bf5a1e2c545e4cf3ea5bc4d36702fe08ca36f
Robin van Steenbergen <robinvansteenbergen@hotmail.com>
2022-07-20 15:40:10 +0200

    added something else

--- aloha.js
+++ aloha.js
@@ -1,3 +1,3 @@
 console.log('Aloha, from Hawaii');
 console.log("Your website is awesome");
-console.log('pardon me for sneaking my greeting in here as well... Aloha to you all and especially to the one and only netninja himself!');
+console.log('pardon me for sneaking my greeting in here as well... Aloha to you all and especially to the one and only netninja himself! much love from the Netherlands');
